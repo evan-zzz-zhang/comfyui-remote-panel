5b188e4a8baf3465f57a99b1ab4b1313b8d1b263
evan-zzz-zhang <ukulele9112@gmail.com>
2026-08-30 21:01:41 +0800

    test(v0.4.4): update release metadata guard

--- tests/test_release_metadata.py
+++ tests/test_release_metadata.py
@@ -12,6 +12,6 @@ def test_package_and_pyproject_versions_match():
     assert pyproject["project"]["version"] == __version__
 
 
-def test_v043_release_metadata_is_not_marked_as_dev_build():
-    assert __version__ == "0.4.3"
+def test_v044_release_metadata_is_not_marked_as_dev_build():
+    assert __version__ == "0.4.4"
     assert "dev" not in __version__.lower()
